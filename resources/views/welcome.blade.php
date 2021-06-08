7aeca7ff1d195edb11240ed06010c2fec9ba06cb
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2021-06-08 18:53:54 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
     <div class="container">
       <div class="header-container d-flex align-items-center justify-content-between">
         <div class="logo">
-          <h1 class="text-light"><a href="index.html"><img src="assets/img/logo1.png" class="img-fluid" alt=""><span>2BGREEN</span></a></h1>
+          <h1 class="text-light"><a href="index.html"><img src="assets/img/logo1.png" class="img-fluid" alt=""><span style="color:#009970;">2BGREEN</span></a></h1>
           <!-- Uncomment below if you prefer to use an image logo -->
           <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
         </div>
