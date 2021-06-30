ce96a181d7db4a2d37ced1b3700f97e1c61789b1
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2021-06-30 21:26:57 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,12 +50,13 @@
 
         <nav id="navbar" class="navbar">
           <ul>
+            <li style="margin-top:15px;"><center><div id="google_translate_element"></div><center></li>
             <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
             <li><a class="nav-link scrollto" href="#about">About</a></li>
             <li><a class="nav-link scrollto " href="#services">services</a></li>
             <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
             <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
-            <center><div id="google_translate_element"></div><center>
+            
           </ul>
           <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
